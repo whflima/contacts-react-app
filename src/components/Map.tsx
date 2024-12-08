bf6d924a324f0cea68ae1573b4bf309cff3f6a82
whflima <wellissonsacer2000@gmail.com>
2024-12-08 09:13:31 -0300

    feat: added environment variables.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,9 +2,11 @@ import { MapProps } from '../interfaces/interfaces';
 import GoogleMapReact from 'google-map-react';
 import { IoLocation } from "react-icons/io5";
 
+const GOOGLE_MAPS_APIKey = process.env.REACT_APP_GOOGLE_MAPS_APIKey as string;
+
 const Marker = (props: any) => {
-    return <IoLocation  size={35}/>
-  }
+    return <IoLocation size={35} />
+}
 
 export default function Map({ latitude, longitude }: MapProps) {
     const defaultProps = {
@@ -18,7 +20,7 @@ export default function Map({ latitude, longitude }: MapProps) {
     return (
         <div style={{ height: '30vh', width: '100%' }}>
             <GoogleMapReact
-                bootstrapURLKeys={{ key: "AIzaSyAlm5FzjzdT911Ep7JvKQJr2imRB8eBFoQ" }}
+                bootstrapURLKeys={{ key: GOOGLE_MAPS_APIKey }}
                 defaultCenter={defaultProps.center}
                 defaultZoom={defaultProps.zoom}
             >
